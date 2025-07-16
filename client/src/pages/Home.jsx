c7c703df9ab9b2d3b21b44938d5ea71ce0ab2ead
moonarmanees <munim.anees.ii@gmail.com>
2025-07-16 01:53:23 +0000

    feat: add React Router (Home/About) and Zustand store (Counter)

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Home</h1>
+      <p>This is the home page of your Vite‑React app.</p>
+    </div>
+  );
+}
