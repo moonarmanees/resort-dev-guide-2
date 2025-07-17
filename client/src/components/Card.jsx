ed08b2b9cfb598737c4390255717fd1e99f6c26f
moonarmanees <munim.anees.ii@gmail.com>
2025-07-17 00:34:58 +0000

    feat: added Card.jsx and cardLis.jsx and updated Home.jsx

---
+++ client/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Card({ title, text }) {
+  return (
+    <div className="card h-100">
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{text}</p>
+      </div>
+    </div>
+  )
+}
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,30 +1,17 @@
 import React from 'react'
 import Header from '../components/Header'
+import CardList from '../components/CardList'
 
 export default function Home() {
-  // sample data
   const items = [
     { title: 'Card One', text: 'This is card number one.' },
     { title: 'Card Two', text: 'This is card number two.' },
     { title: 'Card Three', text: 'This is card number three.' }
   ]
-
   return (
     <div>
       <Header title="Welcome to Home" />
-
-      <div className="row">
-        {items.map((item, i) => (
-          <div key={i} className="col-md-4 mb-4">
-            <div className="card h-100">
-              <div className="card-body">
-                <h5 className="card-title">{item.title}</h5>
-                <p className="card-text">{item.text}</p>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
+      <CardList items={items} />
     </div>
   )
 }
