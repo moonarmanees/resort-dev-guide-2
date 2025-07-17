b5dbbf79ae0dadda0962008bea381b760b18e636
moonarmanees <munim.anees.ii@gmail.com>
2025-07-17 00:22:08 +0000

    feat: add Bootstrap Navbar, enable JS; demo Grid & Cards on Home

---
+++ client/src/components/Layout.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { Outlet, Link } from 'react-router-dom'
+import Header from './Header'
+import Footer from './Footer'
+
+export default function Layout() {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light mb-4">
+        <div className="container">
+          {/* Brand */}
+          <Link className="navbar-brand" to="/">
+            <Header title="My App" />
+          </Link>
+
+          {/* Toggler */}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#mainNavbar"
+            aria-controls="mainNavbar"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          {/* Collapsible links */}
+          <div className="collapse navbar-collapse" id="mainNavbar">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/about">About</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <main className="container">
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  )
+}
