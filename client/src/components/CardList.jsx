b61feb3303c92df1dd0b8ecc81ed73ff7bfc9e12
moonarmanees <munim.anees.ii@gmail.com>
2025-07-17 00:34:58 +0000

    feat: added Card.jsx and cardLis.jsx and updated Home.jsx

---
+++ client/src/components/CardList.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Card from './Card'
+
+export default function CardList({ items }) {
+  return (
+    <div className="row">
+      {items.map((item, idx) => (
+        <div key={idx} className="col-md-4 mb-4">
+          <Card title={item.title} text={item.text} />
+        </div>
+      ))}
+    </div>
+  )
+}
