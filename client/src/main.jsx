d8997fb75446b311f94120f3bfd538971352c0b6
moonarmanees <munim.anees.ii@gmail.com>
2025-07-17 10:09:03 +0000

    fix: isolate context creation & comply with only-export-components rule

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'   // ← add this
-import { CounterProvider } from './context/CounterContext';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+// <-- Make sure you import your provider from the right file:
+import  CounterProvider from './context/CounterProvider.jsx'; // 👉 include the “.jsx” extension
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-   <CounterProvider>
-    <App />
-  </CounterProvider>
+    <CounterProvider>
+      <App />
+    </CounterProvider>
   </BrowserRouter>
-)
-
+);
