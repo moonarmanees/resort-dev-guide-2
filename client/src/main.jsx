b6b44482bb0f371aff440bed04c721edc8407d02
moonarmanees <munim.anees.ii@gmail.com>
2025-07-23 05:21:13 +0000

    fix: support GitHub Pages base path in Vite & React Router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,23 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import CounterProvider from './context/CounterProvider'
+import App from './App'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
-
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-
-
-// <-- Make sure you import your provider from the right file:
-import CounterProvider from "./context/CounterProvider.jsx"; // 👉 include the “.jsx” extension
-
-
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-
-  <BrowserRouter>
+// Vite injects BASE_URL (same as `base` above) into import.meta.env.BASE_URL
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter basename={import.meta.env.BASE_URL}>
     <CounterProvider>
       <App />
     </CounterProvider>
-  </BrowserRouter>,
-);
+  </BrowserRouter>
+)
