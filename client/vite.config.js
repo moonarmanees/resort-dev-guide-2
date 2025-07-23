124abe8ac3f1c61b1ac824f015eca598136257e9
moonarmanees <munim.anees.ii@gmail.com>
2025-07-23 05:00:37 +0000

    perf: add React.lazy/Suspense code‑splitting and bundle visualizer

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './', 
-  plugins: [react()],
+  plugins: [react(),
+    visualizer({
+      filename: 'dist/stats.html',  // where to output the report
+      open: true,                   // auto‑open in browser after build
+    })
+  ],
+  
   build: {
     outDir: '../docs',     // ← this moves dist → docs
     emptyOutDir: true
